5e4c7a504d45d1da54f12d3209439de0c3e20e7f
Nandini Lovanshi <lovanshinandini42@gmail.com>
2025-12-22 19:52:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import SwiggyLanding from "./components/SwiggyLanding.jsx";
 import FoodCollection from "./components/FoodCollection.jsx";
 import InstamartScroll from "./components/InstamartScroll.jsx";
 import DineoutScroll from "./components/DineoutScroll.jsx";
-import AppNow from "./Components/AppNow.jsx";
+import AppNow from "./components/AppNow.jsx";
 import CitiesSection from "./components/CitiesSection.jsx";
 import Footer from "./components/Footer.jsx";
 import Login from "./components/Login.jsx";
@@ -11,28 +11,19 @@ import Signup from "./components/Signup.jsx";
 
 function App() {
   return (
-    <div>
-      <SwiggyLanding />
-      <FoodCollection />
-      <InstamartScroll />
-      <DineoutScroll />
-      <AppNow />
-      <CitiesSection />
-      <Footer />
-      <Login />
-      <Signup />
-      
-      <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </BrowserRouter>
-
-      
-    </div>
+          <div>
+              <SwiggyLanding />
+              <FoodCollection />
+              <InstamartScroll />
+              <DineoutScroll />
+              <AppNow />
+              <CitiesSection />
+              <Footer />
+              <Login />
+              <Signup />
+          </div>
+            
   );
 }
 
 export default App;
-
